117511185bce16554b912f30f3a3cd3981ec08e8
Murtja <murtja.alkhabbaz@uoit.net>
2017-02-12 21:06:44 -0500

    initial commit

--- BikeShareStatistics.py
+++ BikeShareStatistics.py
@@ -10,5 +10,5 @@ def hello():
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    #the host is "localhost" insted of zeros.
+    #the host is "localhost" instead of zeros.
     app.run(host='0.0.0.0', port=port)
