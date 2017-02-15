08f77a9a44088abfeb58c37ad53eb97c4a5858fa
Murtja <murtja.alkhabbaz@uoit.net>
2017-02-14 23:36:35 -0500

    final part

--- BikeShareStatistics.py
+++ BikeShareStatistics.py
@@ -5,33 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-
-    graph_data = {
-        "labels": ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        "datasets": [{
-            "label": '# of Votes',
-            "data": [12, 19, 3, 5, 2, 3],
-            "backgroundColor": [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            "borderColor": [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            "borderWidth": 1
-        }]
-    }
-
-    return render_template('home.html', graph_data=graph_data, name="jojo")
+    return render_template('home.html')
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
--- static/js/main.js
+++ static/js/main.js
@@ -1,33 +1,86 @@
 /**
  * Created by 100478903 on 2017-02-13.
  */
+var map;
+var addRating;
 
-// TODO https://codepen.io/hubpork/pen/xriIz
+$(document).ready(function () {
+    getFirebaseData();
+
+    $("#submitRatingButton").click(function () {
+        var stationId = $("#stationSelectionInput").val();
+        var rating = $("#ratingValue").val();
+
+        addRating(stationId, rating);
+    });
+});
+
+function createSelectBox(data) {
+    var stations = data.val();
+    var outputDiv = $("#stationSelectDiv");
+
+    outputDiv.empty();
+
+    var select = $("<select class='form-control' id='stationSelectionInput' name='stationSelection'>");
+
+    var i;
+    for (i = 0; i < stations.length; i++) {
+        var stationName = stations[i].stationName;
+        var option = $("<option value='" + i + "'>" + stationName + "</option>");
+        select.append(option);
+    }
+
+    outputDiv.append(select);
+}
 
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
         zoom: 14,
         center: createPoint(43.6425662, -79.3892455) // CN Tower
     });
-
-    addMarkers(map);
 }
 
-function getCoordinatesList(){
-    return [
-        createPoint(43.6425662, -79.3892455),
-        createPoint(43.6671223, -79.3956618)
-    ]
+function getFirebaseData() {
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyBxyBeWHvoqud67VAPGjv4nQs--4BEMpQg",
+        authDomain: "bikesharestatistics.firebaseapp.com",
+        databaseURL: "https://bikesharestatistics.firebaseio.com",
+        storageBucket: "bikesharestatistics.appspot.com",
+        messagingSenderId: "306466104920"
+    };
+
+
+    // Initialize the default app
+    var app = firebase.initializeApp(config);
+    var db = app.database();
+
+    db.ref('/stationBeanList/').once('value').then(function (snapshot) {
+        console.log("data retrieve from db");
+        addMarkers(map, snapshot);
+        initGraph(snapshot);
+        createSelectBox(snapshot);
+    });
+
+    function addRatingHandler(stationId, rating) {
+        db.ref('/stationBeanList/' + stationId + '/ratings/').push().set({
+            value: rating
+        });
+        alert ("Thank you for rating");
+    }
+
+    addRating = addRatingHandler;
 }
 
-function addMarkers(map) {
-    var coordinatesList = getCoordinatesList();
+function addMarkers(map, dataList) {
     var markerList = [];
+    var coordinatesList = dataList.val();
 
     var i;
-    for(i=0; i<coordinatesList.length; i++){
+    for (i = 0; i < coordinatesList.length; i++) {
+        var point = createPoint(coordinatesList[i].latitude, coordinatesList[i].longitude);
         var marker = new google.maps.Marker({
-            position: coordinatesList[i],
+            position: point,
             map: map
         });
 
@@ -39,4 +92,85 @@ function addMarkers(map) {
 
 function createPoint(lat, lang) {
     return {lat: lat, lng: lang};
+}
+
+function initGraph(data) {
+    var stations = data.val();
+    var dataObject = {};
+    var labels = [];
+    var bikeAvailabilityData = [];
+    var bikeRatingData = [];
+    var i;
+    for (i = 0; i < stations.length; i++) {
+        labels.push(stations[i].stationName);
+        bikeAvailabilityData.push(stations[i].availableBikes);
+
+        var averageRating = 0;
+        var countRating = 0;
+        var ratings = [];
+        if (stations[i].ratings) {
+            for (var property in stations[i].ratings) {
+                if (stations[i].ratings.hasOwnProperty(property)) {
+                    var value = stations[i].ratings[property].value;
+                    var convertedValue = parseInt(value);
+                    if (convertedValue) {
+                        averageRating = averageRating + convertedValue;
+                        countRating++;
+                    }
+                }
+            }
+            if (countRating > 0) {
+                averageRating = averageRating / countRating;
+            }
+        }
+        bikeRatingData.push(averageRating);
+    }
+    dataObject.labels = labels;
+    dataObject.datasets = [
+        {
+            "label": "Bike Availability",
+            "data": bikeAvailabilityData,
+            "borderWidth": 0
+        }
+    ];
+
+    var ctx = document.getElementById("myChart");
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: dataObject,
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+
+    var ratingDataObject = {};
+    ratingDataObject.labels = labels;
+    ratingDataObject.datasets = [
+        {
+            "label": "Bike Ratings",
+            "data": bikeRatingData,
+            "borderWidth": 0
+        }
+    ];
+
+    ctx = document.getElementById("ratingChart");
+    var ratingChart = new Chart(ctx, {
+        type: 'bar',
+        data: ratingDataObject,
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
 }
